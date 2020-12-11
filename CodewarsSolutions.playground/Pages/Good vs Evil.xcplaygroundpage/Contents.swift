dbfd5b097770db51b5c56893ed2de257ae8dfc66
Thiago Oliveira <thiago@outlook.com.br>
2020-12-10 23:21:39 -0300

    Reduced returns

--- CodewarsSolutions.playground/Pages/Good vs Evil.xcplaygroundpage/Contents.swift
+++ CodewarsSolutions.playground/Pages/Good vs Evil.xcplaygroundpage/Contents.swift
@@ -16,13 +16,7 @@ func evaluate(good: String, vsEvil evil: String) -> String {
         element * evilWorths[index]
     }.reduce(0, +)
     
-    if totalGoodWorths > totalEvilWorths {
-        return "Battle Result: Good triumphs over Evil"
-    } else if totalGoodWorths == totalEvilWorths {
-        return "Battle Result: No victor on this battle field"
-    }
-
+    if totalGoodWorths > totalEvilWorths { return "Battle Result: Good triumphs over Evil" }
+    else if totalGoodWorths == totalEvilWorths { return "Battle Result: No victor on this battle field" }
     return "Battle Result: Evil eradicates all trace of Good"
 }
-
-print(evaluate(good: "1 1 1 1 1 1", vsEvil: "0 1 1 1 1 1 1"))
